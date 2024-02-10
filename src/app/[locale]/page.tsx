ad3fa179bc068f89a43ae673d1582fe58719ef52
Cristhian Melo <rxtsel@hotmail.com>
2024-02-10 15:00:02 -0500

    chore: remove toggle theme for root page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,10 @@
 import { useTranslations } from 'next-intl'
-import { ToggleTheme } from '@/components'
 
 export default function Home() {
   const t = useTranslations('site')
   return (
     <>
       <h1 className='dark:text-white text-black'> {t('title')}</h1>
-
-      <ToggleTheme />
     </>
   )
 }
