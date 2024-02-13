6d6ce0dc96d1e0a0a7a94223297df89579824dfd
Cristhian Melo <rxtsel@hotmail.com>
2024-02-13 15:30:36 -0500

    chore: add private pages

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -4,5 +4,8 @@ export const ROUTES = {
     login: '/auth/sign-in',
     register: '/auth/sign-up',
     forgotPassword: '/auth/forgot-password'
+  },
+  private: {
+    dashboard: '/dashboard'
   }
 }
