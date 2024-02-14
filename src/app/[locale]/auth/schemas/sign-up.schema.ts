5ae7b0f684ce885c0d835b0a76f8fb21cc8c289e
Cristhian Melo <rxtsel@hotmail.com>
2024-02-14 02:07:28 -0500

    chore: add min pasword to 6 characters

--- src/app/[locale]/auth/schemas/sign-up.schema.ts
+++ src/app/[locale]/auth/schemas/sign-up.schema.ts
@@ -14,15 +14,15 @@ export const formSignUpSchema = (t: T) =>
       }),
       password: z
         .string()
-        .min(5, {
-          message: t('messages.minCharacters', { name: t('messages.password').toLowerCase(), qty: 5 })
+        .min(6, {
+          message: t('messages.minCharacters', { name: t('messages.password').toLowerCase(), qty: 6 })
         })
         .max(100, {
           message: t('messages.maxCharacters', { name: t('messages.password').toLowerCase(), qty: 100 })
         })
         .refine(
           value => {
-            return /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,100}$/.test(value)
+            return /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,100}$/.test(value)
           },
           {
             message: t('messages.passwordMatch')
