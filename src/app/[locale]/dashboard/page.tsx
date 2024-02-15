f212d9f2e9f2705a45d3a3a69dbb575b1b65bf8b
Cristhian Melo <rxtsel@hotmail.com>
2024-02-14 19:28:43 -0500

    chore: change dashboard base data

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -6,11 +6,17 @@ import type { Database } from '@/types/database.types'
 const Dashboard = async () => {
   const cookieStore = cookies()
   const supabase = createServerComponentClient<Database>({ cookies: () => cookieStore })
-  const { data } = await supabase.from('workspaces').select()
+  const { data } = await supabase.from('users').select()
   return (
     <>
-      <h1 className='dark:text-white text-black text-6xl font-medium font-sans'>Welcome Back</h1>
+      <h1 className='dark:text-white text-black text-2xl lg:text-6xl font-medium font-sans'>Welcome Back! 🌟</h1>
+
+      <h2 className='dark:text-white text-black text-2xl font-sans my-5'>Your data:</h2>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+
+      <strong className='mt-5 lg:mt-10 block'>
+        This project is currently a work in progress. Please check back later for updates. Thank you! ✌️
+      </strong>
     </>
   )
 }
