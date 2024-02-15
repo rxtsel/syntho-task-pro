2ff8eb324f4fc0bcbc798b0708c51f50f4dc5a70
Cristhian Melo <rxtsel@hotmail.com>
2024-02-14 19:21:26 -0500

    fix: bug fix types

--- src/app/[locale]/dashboard/components/user-nav.tsx
+++ src/app/[locale]/dashboard/components/user-nav.tsx
@@ -13,18 +13,24 @@ import { cookies } from 'next/headers'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
 import type { Database } from '@/types/database.types'
-import { toast } from 'sonner'
 import { LogoutButton } from './logout-button'
+import { toast } from 'sonner'
 
 export const UserNav = async () => {
   const cookieStore = cookies()
   const supabase = createServerComponentClient<Database>({ cookies: () => cookieStore })
   const { data: dataSession } = await supabase.auth.getSession()
-  const { data } = await supabase.from('users').select('*').eq('id', dataSession.session?.user.id)
 
-  const { email, avatar_url, name, user_name } = data[0]
+  if (!dataSession.session) return toast.error('You are not logged in')
 
-  const nameController = user_name ? user_name : name ? name : email.split('@')[0]
+  const { data } = await supabase.from('users').select().eq('id', dataSession.session.user.id)
+
+  if (!data) return toast.error('User not found')
+
+  const { email, avatar_url, name, user_name } = data[0] as Database['public']['Tables']['users']['Row']
+
+  const nameController = user_name ? user_name : name ? name : email ? email.split('@')[0] : 'User'
+  const avatarController = avatar_url ? avatar_url : name ? name.charAt(0) : email?.charAt(0)
 
   if (dataSession.session) {
     return (
@@ -32,11 +38,8 @@ export const UserNav = async () => {
         <DropdownMenuTrigger asChild>
           <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
             <Avatar className='h-8 w-8'>
-              <AvatarImage
-                src={avatar_url ? avatar_url : name ? name.charAt(0) : email?.charAt(0)}
-                alt={avatar_url ? avatar_url : name ? name.charAt(0) : email?.charAt(0)}
-              />
-              <AvatarFallback>{avatar_url ? avatar_url : name ? name.charAt(0) : email?.charAt(0)}</AvatarFallback>
+              <AvatarImage src={avatarController} alt={avatarController} />
+              <AvatarFallback>{avatarController}</AvatarFallback>
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
